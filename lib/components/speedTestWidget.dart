cb2799e101df2e68277cde5787f896f1380690ac
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2023-12-02 16:57:29 -0600

    localStorage terminado

--- lib/components/speedTestWidget.dart
+++ lib/components/speedTestWidget.dart
@@ -1,6 +1,7 @@
 import 'package:flare_flutter/flare_controls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_internet_speed_test/flutter_internet_speed_test.dart';
+import 'package:localstorage/localstorage.dart';
 
 import '../services/speedTest.dart';
 
@@ -104,6 +105,8 @@ class _SpeedTestWidgetState extends State<SpeedTestWidget> {
                 _uploadCompletionTime = upload.durationInMillis;
                 _testInProgress = false;
               });
+
+              saveTestData(download, upload);
             },
             onProgress: (double percent, TestResult data) {
               setState(() {
@@ -154,6 +157,21 @@ class _SpeedTestWidgetState extends State<SpeedTestWidget> {
         }
       },
     );
+  }
+
+  void saveTestData(TestResult download, TestResult upload) async {
+    final localStorage = LocalStorage('speed_test_data');
+    await localStorage.ready;
+
+    List<dynamic> history = localStorage.getItem('history') ?? [];
+    history.add({
+      'date': DateTime.now().toString(),
+      'downloadSpeed': download.transferRate,
+      'uploadSpeed': upload.transferRate,
+      'ip': _ip,
+    });
+
+    localStorage.setItem('history', history);
   }
 
   void reset() {
